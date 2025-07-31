313890ecd4329f0e28ef58934a10fc6b596e77d7
Krish Koria <krishkoria2004@gmail.com>
2025-07-31 15:54:23 +0530

    refactor: remove CreateSong component and its associated functionality

--- frontend/src/components/create.tsx
+++
@@ -1,6 +0,0 @@
-import { queueSong } from "@/lib/actions";
-import { Button } from "./ui/button";
-
-export default function CreateSong() {
-  return <Button onClick={queueSong}>Create Song</Button>;
-}
